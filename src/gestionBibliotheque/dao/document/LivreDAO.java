d741b055415df1c7f247f1dac544810873319ac3
OMAZER28 <omarmazer@gmail.com>
2020-06-06 13:30:08 +0100

    v3

---
+++ src/gestionBibliotheque/dao/document/LivreDAO.java
@@ -0,0 +1,142 @@
+package gestionBibliotheque.dao.document;
+
+import java.util.*;
+import java.sql.*;
+import java.io.*;
+
+import gestionBibliotheque.dao.DAOException;
+import gestionBibliotheque.model.documents.Livre;
+import gestionBibliotheque.model.utilisateurs.Etudiant;
+
+
+public class LivreDAO {
+	
+	private Connection connection;
+	
+	public LivreDAO() throws DAOException {
+		
+		Properties dbprops = new Properties();
+		try {
+			dbprops.load(new FileInputStream("dbprops.properties"));
+		} catch(IOException e) {
+			throw new DAOException("problème de chargement des informations de la base de donnée...", e);
+		}
+		String user, password, dburl;
+		user = dbprops.getProperty("user");
+		password = dbprops.getProperty("password");
+		dburl = dbprops.getProperty("dburl");
+		
+		
+		try {
+			connection = DriverManager.getConnection(dburl, user, password);
+		} catch(SQLException e) {
+			throw new DAOException("problème de connexion au base de donnée " + dburl + "...", e);
+		}
+		
+		System.out.println("DB connection successful to: " + dburl);
+	}
+	
+	public void addLivre(Livre l) throws DAOException{
+		PreparedStatement req = null;
+		String sql;
+		try {
+			sql = "INSERT INTO livre (id, titre, nomEditeur, auteur) VALUES (0, ?, ?, ?) ";
+			req = connection.prepareStatement(sql);
+			req.setString(1, l.getTitre());
+			req.setString(2, l.getNomEditeur());
+			req.setString(3, l.getAuteur());
+			req.executeUpdate();
+			
+		} catch(SQLException e) {
+			throw new DAOException("problème d'ajout d'un livre " + l.getTitre(), e);
+		}finally {
+			close(req);
+		}
+	}
+	
+	public void deleteLivre(Livre l) throws DAOException{
+		PreparedStatement req = null;
+		String sql;
+		try {
+			sql = "DELETE FROM livre WHERE titre=? and nomEditeur=? and auteur=?";
+			req = connection.prepareStatement(sql);
+			req.setString(1, l.getTitre());
+			req.setString(2, l.getNomEditeur());
+			req.setString(3, l.getAuteur());
+			req.executeUpdate();
+			
+		} catch(SQLException e) {
+			throw new DAOException("problème de suppression d'un livre " + l.getTitre(), e);
+		}finally {
+			close(req);
+		}
+	}
+	
+	
+	public List<Livre> getAllLivre() throws DAOException {
+		List<Livre> list = new ArrayList<Livre>();
+		
+		Statement req = null;
+		ResultSet rs = null;
+		String sql;
+		
+		try {
+			req = connection.createStatement();
+			sql = "SELECT * FROM livre ";
+			rs = req.executeQuery(sql);
+			
+			while (rs.next()) {
+				int id = rs.getInt("id");
+				String titre = rs.getString("titre");
+				String nomEditeur = rs.getString("nomEditeur");
+				String auteur = rs.getString("auteur");
+				
+				Livre l = new Livre(id,titre,nomEditeur,auteur);
+				list.add(l);
+			}
+
+			return list;		
+		}catch (SQLException e) {
+			throw new DAOException("problème d'affichage de la liste des livres ", e);
+		}finally {
+			close(req, rs);
+		}
+	}
+	
+	private static void close(Connection myConn, Statement myStmt, ResultSet myRs) throws DAOException {
+
+		if (myRs != null) {
+			try {
+				myRs.close();
+			} catch (SQLException e) {
+				throw new DAOException("Error closing the ResultSet ... ", e);
+			}
+		}
+
+		if (myStmt != null) {
+			try {
+				myStmt.close();
+			} catch (SQLException e) {
+				throw new DAOException("Error closing the Statement ... ", e);
+			}
+		}
+		
+		if (myConn != null) {
+			try {
+				myConn.close();
+			} catch (SQLException e) {
+				throw new DAOException("Error closing the Connection ... ", e);
+			}
+		}
+	}
+	
+	private void close(Statement myStmt, ResultSet myRs) throws DAOException {
+		close(null, myStmt, myRs);		
+	}
+
+	private void close(Statement myStmt) throws DAOException {
+		close(null, myStmt, null);		
+	}
+
+}
+
